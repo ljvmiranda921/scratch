91ecc482de11581bf16a1b76bb9877aeb6ecbef1
Lj Miranda <ljvmiranda@gmail.com>
2022-06-28 11:00:10 +0800

    Add prettymaps and font download script

---
+++ 2022-06-28-prettymaps/download_fonts.sh
@@ -0,0 +1,4 @@
+mkdir prints
+mkdir assets
+mkdir assets/Permanent_Marker/
+wget -O assets/Permanent_Marker/PermanentMarker-Regular.ttf https://github.com/marceloprates/prettymaps/raw/main/assets/Permanent_Marker/PermanentMarker-Regular.ttf
