659b85d46d40b7e79b8ee6b0aff6649e318a08c0
Lj Miranda <ljvmiranda@gmail.com>
2023-03-10 14:56:35 +0800

    Implement stab2018 parser

--- 2023-03-03-langchain/scripts/parsers.py
+++ 2023-03-03-langchain/scripts/parsers.py
@@ -2,19 +2,24 @@
 
 Each annotation guideline has their own set of labels. We want to parse them
 properly. GPT-3 is still an LLM, so it can return any arbitrary string of text.
+I tried to be robust, but for some cases I just decided that a naive parser
+will work.
+
+Although some tasks are binary classification problems. I opted to parse them as
+an exclusive multilabel classification problem.
 """
 
-from typing import Callable, Dict
+from typing import Any, Callable, Dict, List, Optional
 
 from pathlib import Path
 from prodigy.util import msg
+from prodigy.types import TaskType
 
 
 def get_parser(file: Path) -> Callable:
     if file.stem not in PARSERS.keys():
         msg.fail(f"Cannot find parser for {file}. Available: {PARSERS.keys()}", exit=1)
-
-    parser = PARSERS.get(file.name)
+    parser = PARSERS.get(file.stem)
     return parser
 
 
@@ -30,8 +35,23 @@ def _parse_shnarch2018():
     pass
 
 
-def _parse_stab2018():
-    pass
+def _parse_stab2018(
+    response: str,
+    example: Optional[TaskType] = None,
+    labels: List[str] = ["Non-argument", "Supporting argument", "Opposing argument"],
+) -> Dict[str, Any]:
+    for label in labels:
+        if label.lower() in response.strip().lower():
+            accept = label
+            break
+        else:
+            msg.warn(f"Cannot parse: '{response}'. Will set to 'Non-argument'")
+            accept = "Non-argument"
+    return {
+        "options": [{"id": label, "text": label} for label in labels],
+        "answer": "accept",
+        "accept": [accept],
+    }
 
 
 PARSERS: Dict[str, Callable] = {
