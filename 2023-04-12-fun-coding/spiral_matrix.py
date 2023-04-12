0ca6330b80b270f83064ff055a55fba346448616
Lj Miranda <ljvmiranda@gmail.com>
2023-04-12 21:27:38 +0800

    [wip] Update

---
+++ 2023-04-12-fun-coding/spiral_matrix.py
@@ -0,0 +1,14 @@
+problem_1 = """
+Given an m x n matrix, return all elements of the matrix in spiral order
+Input: matrix = [[1,2,3], [4,5,6], [7.8.9]]
+Output: [1,2,3,69,8,7,4,5]
+"""
+
+def solution_1():
+    print(problem_1)
+    pass
+
+
+
+if __name__ == "__main__":
+    solution_1()
