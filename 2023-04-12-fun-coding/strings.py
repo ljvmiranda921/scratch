9943639440a206a4cc6586ce621b7ec53cbea1ec
Lj Miranda <ljvmiranda@gmail.com>
2023-04-15 14:11:00 +0800

    [wip] Update

---
+++ 2023-04-12-fun-coding/strings.py
@@ -0,0 +1,28 @@
+from wasabi import msg
+from collections import Counter
+
+
+def variation_1():
+    problem = """
+    Given two strings s and t, write a function to determine if t is an anagram
+    of s.
+    Input: s = "anagram", t = "nagaram"
+    Output: true
+
+    Input: s = "rat" t = "car"
+    Output: false
+    """
+
+    print(problem)
+
+    def solution(s: str, t: str) -> bool:
+        # Slower sol'n sorted(s) == sorted(t)
+        return Counter(s) == Counter(t)
+
+    print(solution(s="anagram", t="nagaram"))
+    print(solution(s="rat", t="car"))
+
+
+if __name__ == "__main__":
+    variation_1()
+    msg.divider()
