530e0b4c8495812049acd2fadd75da483e9c5535
Lj Miranda <ljvmiranda@gmail.com>
2023-10-15 19:41:10 +0800

    [wip] Update

--- 2023-10-15-embeddings/scripts/plot.py
+++ 2023-10-15-embeddings/scripts/plot.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Tuple
 
 
 import spacy
@@ -8,6 +8,7 @@ import typer
 import numpy as np
 from spacy.tokens import Doc, DocBin
 from srsly import write_msgpack
+from tqdm import tqdm
 from wasabi import msg
 
 Arg = typer.Argument
@@ -19,17 +20,19 @@ class Example:
     """Class for keeping track of an example"""
 
     text: str
+    span_text: str
     label: str
-    char_start: int
-    char_end: int
+    start_char: int
+    end_char: int
     ctx_vector: np.ndarray
-    tsne_coord: np.ndarray
 
     # Other properties, assign the first applicable category
     paren: bool  # if preceded by a left parenthesis
     all_caps: bool  # if it consists of all capitals
     initial: bool  # if it is the first token in a sentence
     plain: bool  # catch-all category
+
+    tsne_coord: Tuple[float, float] = (0, 0)
 
 
 def plot(
@@ -42,9 +45,41 @@ def plot(
     # Read file
     nlp = spacy.blank("xx")
     doc_bin = DocBin().from_disk(embeddings)
-    docs = list(doc_bin.get_docs(nlp.vocab))
-    msg.good(f"Found {len(docs)} from {embeddings}")
+    docs = doc_bin.get_docs(nlp.vocab)
 
     # Format documents into a human-readable table
-    for doc in docs:
-        for ent in doc.ents
+    examples = []
+    for doc in tqdm(docs):
+        for ent in doc.ents:
+            # We only want the first applicable category for
+            # per-label plots. This looks a bit ugly.
+            if ent.start != 0 and doc[ent.start - 1].text == "(":
+                props = (1, 0, 0, 0)
+            elif all([token.is_upper for token in ent]):
+                props = (0, 1, 0, 0)
+            elif ent.start == 0:
+                props = (0, 0, 1, 0)
+            else:
+                props = (0, 0, 0, 0)
+
+            eg = Example(
+                text=doc.text,
+                span_text=ent.text,
+                label=ent.label_,
+                start_char=ent.start_char,
+                end_char=ent.end_char,
+                ctx_vector=doc.user_data[
+                    ("._.", "ctx_vector", ent.start_char, ent.end_char)
+                ],
+                paren=props[0],
+                all_caps=props[1],
+                initial=props[2],
+                plain=props[3],
+            )
+            examples.append(eg)
+
+    msg.info(f"Processed {len(examples)} entities from {embeddings}")
+
+
+if __name__ == "__main__":
+    typer.run(plot)
