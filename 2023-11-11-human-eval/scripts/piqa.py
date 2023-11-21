0377b9a6fc71625e315bca987a9500328a687ada
Lj Miranda <ljvmiranda@gmail.com>
2023-11-21 14:23:39 +0800

    Use better names

--- 2023-11-11-human-eval/scripts/piqa.py
+++ 2023-11-11-human-eval/scripts/piqa.py
@@ -79,7 +79,7 @@ def evaluate(
 ):
     """Compare results on gold-standard data."""
 
-    def _to_spacy_doc(
+    def _make_doc(
         nlp: "spacy.language.Language",
         record: Dict[str, Any],
         label: str,
@@ -96,7 +96,7 @@ def evaluate(
     ref_records = list(srsly.read_jsonl(references))
     ref_labels = [rec.get("meta").get("label") for rec in ref_records]
     ref_docs = [
-        _to_spacy_doc(nlp, rec, label, CLASS_LABELS)
+        _make_doc(nlp, rec, label, CLASS_LABELS)
         for rec, label in zip(ref_records, ref_labels)
     ]
 
@@ -104,7 +104,7 @@ def evaluate(
     pred_records = list(srsly.read_jsonl(predictions))
     pred_labels = list([rec.get("accept")[0] for rec in pred_records])
     pred_docs = [
-        _to_spacy_doc(nlp, rec, label, CLASS_LABELS)
+        _make_doc(nlp, rec, label, CLASS_LABELS)
         for rec, label in zip(pred_records, pred_labels)
     ]
 
