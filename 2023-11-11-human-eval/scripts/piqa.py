412f6fa2bda3e6e6ab68d19c0fb75a99bc0bc694
Lj Miranda <ljvmiranda@gmail.com>
2023-11-21 10:47:28 +0800

    Implement evals for PIQA

--- 2023-11-11-human-eval/scripts/piqa.py
+++ 2023-11-11-human-eval/scripts/piqa.py
@@ -1,10 +1,19 @@
 from enum import Enum
 from pathlib import Path
+from typing import Dict, List, Any
 
 import srsly
+import spacy
 import typer
+from spacy.tokens import Doc
+from spacy.scorer import Scorer
+from spacy.training import Example
 from datasets import load_dataset
 from wasabi import msg
+
+app = typer.Typer()
+
+CLASS_LABELS = ["sol1", "sol2"]
 
 
 class Interface(str, Enum):
@@ -18,7 +27,8 @@ class Split(str, Enum):
     validation = "validation"
 
 
-def download_piqa(
+@app.command()
+def download(
     # fmt: off
     output_path: Path = typer.Argument(..., help="Path to save the JSONL file."),
     split: Split = typer.Option(Split.validation, help="Dataset split to convert."),
@@ -38,7 +48,7 @@ def download_piqa(
                         {"id": "sol1", "text": eg.get("sol1")},
                         {"id": "sol2", "text": eg.get("sol2")},
                     ],
-                    "meta": {"label": eg.get("label")},
+                    "meta": {"label": CLASS_LABELS[eg.get("label")]},
                 }
             )
         elif interface == Interface.textbox.value:
@@ -50,7 +60,7 @@ def download_piqa(
                     "field_rows": 5,
                     "field_placeholder": "Type here...",
                     "field_autofocus": False,
-                    "meta": {"label": eg.get("label")},
+                    "meta": {"label": CLASS_LABELS[eg.get("label")]},
                 }
             )
         else:
@@ -60,5 +70,52 @@ def download_piqa(
     msg.good(f"Saved {len(annotation_tasks)} annotation tasks to {output_path}")
 
 
+@app.command()
+def evaluate(
+    # fmt: off
+    references: Path = typer.Argument(..., help="Path to the gold-standard data."),
+    predictions: Path = typer.Argument(..., help="Path to the human-annotated results."),
+    # fmt: on
+):
+    """Compare results on gold-standard data."""
+    nlp = spacy.blank("en")
+
+    # Get reference documents
+    ref_records = list(srsly.read_jsonl(references))
+    ref_labels = [rec.get("meta").get("label") for rec in ref_records]
+    ref_docs = [
+        to_spacy_doc(nlp, rec, label, CLASS_LABELS)
+        for rec, label in zip(ref_records, ref_labels)
+    ]
+
+    # Get predicted documents
+    pred_records = list(srsly.read_jsonl(predictions))
+    pred_labels = list([rec.get("accept")[0] for rec in pred_records])
+    pred_docs = [
+        to_spacy_doc(nlp, rec, label, CLASS_LABELS)
+        for rec, label in zip(pred_records, pred_labels)
+    ]
+
+    # Create spacy Examples
+    examples = [Example(pred, ref) for pred, ref in zip(pred_docs, ref_docs)]
+    msg.text(f"Found {len(examples)} examples")
+    scores = Scorer.score_cats(
+        examples, attr="cats", labels=CLASS_LABELS, multi_label=False
+    )
+    msg.text(title="Scores", text=scores)
+
+
+def to_spacy_doc(
+    nlp: "spacy.language.Language",
+    record: Dict[str, Any],
+    label: str,
+    class_labels: List[str],
+) -> Doc:
+    doc = nlp.make_doc(record.get("text"))
+    doc.cats = {class_label: 0 for class_label in set(class_labels)}
+    doc.cats[label] = 1
+    return doc
+
+
 if __name__ == "__main__":
-    typer.run(download_piqa)
+    app()
