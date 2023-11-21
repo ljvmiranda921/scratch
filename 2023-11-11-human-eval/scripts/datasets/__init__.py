36baa0d3d5127641d246960601a853572c44f3eb
Lj Miranda <ljvmiranda@gmail.com>
2023-11-21 21:08:01 +0800

    Update dataset dictionary

--- 2023-11-11-human-eval/scripts/datasets/__init__.py
+++ 2023-11-11-human-eval/scripts/datasets/__init__.py
@@ -3,16 +3,19 @@ from enum import Enum
 from .hellaswag import HellaSwagDataset
 from .piqa import PIQADataset
 from .winogrande import WinograndeDataset
+from .lambada import LAMBADADataset
 
 
 class Dataset(str, Enum):
     piqa = "piqa"
     hellaswag = "hellaswag"
     winogrande = "winogrande"
+    lambada = "lambada"
 
 
 DATASETS = {
     Dataset.piqa.value: PIQADataset,
     Dataset.hellaswag.value: HellaSwagDataset,
     Dataset.winogrande.value: WinograndeDataset,
+    Dataset.lambada.value: LAMBADADataset,
 }
