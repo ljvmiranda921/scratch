cf1ff2c85d5bf0d754fb7eb5a753b1264c4d0e3e
Lj Miranda <ljvmiranda@gmail.com>
2023-12-13 13:04:56 +0800

    Fix doc_to_target

--- 2023-11-11-human-eval/custom_harness_tasks/logiqa_custom/utils_logiqa.py
+++ 2023-11-11-human-eval/custom_harness_tasks/logiqa_custom/utils_logiqa.py
@@ -21,5 +21,4 @@ def doc_to_text(eg) -> str:
 
 def doc_to_target(eg) -> int:
     doc = eg["meta"]["doc"]
-    choices = ["a", "b", "c", "d"]
-    return choices.index(doc["correct_option"].strip())
+    return int(doc["correct_option"])
