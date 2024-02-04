4b27f8714396c26ce17865dfd7924b08d1777394
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2024-02-03 22:24:04 -0800

    Add NewsPH EDA (#24)

---
+++ 2023-12-19-ud-eda/README.md
@@ -0,0 +1,36 @@
+<!-- WEASEL: AUTO-GENERATED DOCS START (do not remove) -->
+
+# 🪐 Weasel Project: Tagalog UD EDA
+
+EDA for Tagalog Universal Dependencies
+
+
+## 📋 project.yml
+
+The [`project.yml`](project.yml) defines the data assets required by the
+project, as well as the available commands and workflows. For details, see the
+[Weasel documentation](https://github.com/explosion/weasel).
+
+### ⏯ Commands
+
+The following commands are defined by the project. They
+can be executed using [`weasel run [name]`](https://github.com/explosion/weasel/tree/main/docs/cli.md#rocket-run).
+Commands are only re-run if their inputs have changed.
+
+| Command | Description |
+| --- | --- |
+| `setup-data` | Unzip CoNLLu data |
+| `validate-all` | Run validator script to all ConLLU files and store their results into outputs |
+
+### 🗂 Assets
+
+The following assets are defined by the project. They can
+be fetched by running [`weasel assets`](https://github.com/explosion/weasel/tree/main/docs/cli.md#open_file_folder-assets)
+in the project directory.
+
+| File | Source | Description |
+| --- | --- | --- |
+| [`assets/conllu-verified.zip`](assets/conllu-verified.zip) | Local | Updated ConLLU files containing UD Tagalog data |
+| `assets/tools` | Git | Validation script for Universal Dependencies |
+
+<!-- WEASEL: AUTO-GENERATED DOCS END (do not remove) -->
---
+++ 2023-12-19-ud-eda/scripts/validate_conllu_bulk.py
@@ -0,0 +1,67 @@
+import sys
+import re
+import subprocess
+from pathlib import Path
+from typing import Dict, List
+
+import typer
+import polars as pl
+from wasabi import msg
+from tqdm import tqdm
+
+
+def validate_conllu_bulk(
+    # fmt: off
+    input_dir: Path = typer.Argument(..., help="Directory to search for CoNLLu files."),
+    output_dir: Path = typer.Argument(..., help="Directory to save the logs as JSONL."),
+    validator_script: Path = typer.Argument(..., help="Path to the Universal Dependencies validator script."),
+    verbose: bool = typer.Option(False, help="Display more information."),
+    # fmt: on
+):
+    for dir in sorted(input_dir.iterdir()):
+        conllu_files = sorted(list(dir.glob("*.conllu")))
+        msg.info(f"Found {len(conllu_files)} files from {dir}")
+
+        for file in tqdm(conllu_files):
+            # Run validator and parse the results
+            result = subprocess.run(
+                f"cat {file} | {sys.executable} {validator_script} --lang tl --max-err=0",
+                capture_output=True,
+                shell=True,
+            )
+            raw = result.stderr.decode("utf-8")
+            msg.text(raw, show=verbose)
+            parsed_output = parse_error_log(raw)
+
+            # Save the parsed results
+            df = pl.DataFrame(parsed_output)
+            (output_dir / file.parent.name).mkdir(parents=True, exist_ok=True)
+            output_file = output_dir / file.parent.name / f"{file.stem}.csv"
+            df.write_csv(output_file)
+
+
+def parse_error_log(raw: str) -> List[Dict[str, str]]:
+    """Parse the raw error log and transform it into a parseable sequence
+
+    [Line 14 Sent 2]: [L4 Morpho feature-value-upos-not-permitted] Value remt of feature
+    |--- location --|  |--------------- error type ---------------| |--- description---|
+    """
+    pattern = r"\[([^\]]+)\]: \[([^\]]+)\] (.+)"
+    lines = raw.splitlines()
+
+    parsed_outputs = []
+    for line in lines:
+        match = re.match(pattern, line)
+        if match:
+            parsed_outputs.append(
+                {
+                    "location": match.group(1),
+                    "error_code": match.group(2),
+                    "error_description": match.group(3),
+                }
+            )
+    return parsed_outputs
+
+
+if __name__ == "__main__":
+    typer.run(validate_conllu_bulk)
