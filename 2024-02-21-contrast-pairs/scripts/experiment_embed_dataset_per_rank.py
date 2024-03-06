832ceb8e2b5862d6b85d594eb4703292f72f16fd
Lj Miranda <ljvmiranda@gmail.com>
2024-03-05 16:40:44 -0800

    [wip] Update

--- 2024-02-21-contrast-pairs/scripts/experiment_embed_dataset_per_rank.py
+++ 2024-02-21-contrast-pairs/scripts/experiment_embed_dataset_per_rank.py
@@ -13,7 +13,7 @@ from scripts.preprocessors import DATASET_PREPROCESSORS
 app = typer.Typer()
 
 ranked_datasets = [
-    "openai/summarize_from_feedback",
+    # "openai/summarize_from_feedback",
     "stanford/SHP",
     "berkeley-nest/Nectar",
 ]
@@ -29,7 +29,7 @@ def embed():
     for dataset_name in ranked_datasets:
         msg.divider(f"Embedding dataset: {dataset_name}")
         chosen, _ = DATASET_PREPROCESSORS.get(dataset_name)()
-        ranks = ["next", "mid", "last"]
+        ranks = ["last", "mid", "next"]
 
         if dataset_name == "berkeley-nest/Nectar":
             ranks += list(range(1, 7 + 1))
@@ -67,7 +67,69 @@ def embed():
 @app.command("visualize")
 def visualize():
     """Visualize cosine distance distribution per rank."""
-    pass
+    data_dir = Path("embeddings/get-dist-ranking")
+    output_dir = Path("outputs")
+
+    for dataset_name in ranked_datasets:
+        msg.text(f"Visualizing {dataset_name}...")
+        dataset_dir = data_dir / dataset_name.replace("/", "___")
+
+        embeddings = {}
+        ranks = ["last", "mid", "next"]
+
+        embeddings["chosen"] = np.load(list(dataset_dir.glob("chosen.npy"))[0])
+        for rank in ranks:
+            embeddings[f"rejected_{rank}"] = np.load(
+                list(dataset_dir.glob(f"rejected_{rank}.npy"))[0]
+            )
+
+        hist_data = []
+        group_labels = []
+        for rank in ranks:
+            group_labels.append(f"chosen and rejected ({rank})")
+            hist_data.append(
+                [
+                    cosine(chosen, rejected)
+                    for chosen, rejected in zip(
+                        embeddings["chosen"], embeddings[f"rejected_{rank}"]
+                    )
+                ]
+            )
+        breakpoint()
+
+        layout_properties = {
+            "autosize": False,
+            "width": 720,
+            "height": 540,
+            "font_family": "CMU Sans Serif",
+            "title_font_family": "CMU Sans Serif",
+            "title_font_size": 24,
+            "title_text": dataset_name,
+            "paper_bgcolor": "rgba(0,0,0,0)",
+            "plot_bgcolor": "rgba(0,0,0,0)",
+            "xaxis_title": "Cosine distance",
+            "yaxis_title": "Probability density",
+            "legend_traceorder": "reversed",
+        }
+
+        fig = create_distplot(
+            hist_data,
+            group_labels,
+            bin_size=0.1,
+            show_rug=False,
+        )
+        fig.update_layout(
+            legend={
+                "title": "Pairs",
+                "orientation": "h",
+                "yref": "paper",
+                "y": -0.3,
+            },
+            **layout_properties,
+        )
+
+        outfile = "distance_rank_plot_" + dataset_name.replace("/", "___") + ".html"
+        fig.write_html(output_dir / outfile, include_plotlyjs="cdn")
 
 
 if __name__ == "__main__":
