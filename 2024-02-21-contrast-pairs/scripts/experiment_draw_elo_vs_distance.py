4a9c85778c791706615885204777596e3f70738b
Lj Miranda <ljvmiranda@gmail.com>
2024-03-08 13:19:26 -0800

    [wip] Update

--- 2024-02-21-contrast-pairs/scripts/experiment_draw_elo_vs_distance.py
+++ 2024-02-21-contrast-pairs/scripts/experiment_draw_elo_vs_distance.py
@@ -43,15 +43,38 @@ def _get_text_ratings(dataset_name: str, model: SentenceTransformer):
 
         return pd.DataFrame(rows, columns=["text", "elo_ratings", "dist_from_chosen"])
 
+    if dataset_name == "stanfordnlp/SHP":
+        dataset = load_dataset("stanfordnlp/SHP", split="train").filter(
+            lambda x: x["domain"] == "explainlikeimfive_train"
+        )
+
+        dataset_df = dataset.to_pandas()
+        rows: List[str, float, float] = []
+        for _, instances in tqdm(dataset.groupby("post_id")):
+            matchups = [
+                (instance["human_ref_A"], instance["human_ref_B"], instance["labels"])
+                for _, instance in instances.iterrows()
+            ]
+            ranked = compute_elo_rankings(matchups)
+            chosen_text, _ = ranked[0]
+            chosen_emb = model.encode(chosen_text)
+            for text, elo_ratings in ranked:
+                text_emb = model.encode(text, normalize_embeddings=True)
+                dist_from_chosen = cosine(chosen_emb, text_emb)
+                rows.append([text, elo_ratings, dist_from_chosen])
+
+        return pd.DataFrame(rows, columns=["text", "elo_ratings", "dist_from_chosen"])
+
 
 @app.command("embed")
 def embed():
+    """Embed datasets, compute distance, and elo ranking"""
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device=device)
     datasets = [
-        "openai/summarize_from_feedback",
-        "stanford/SHP",
-        "berkeley-nest/Nectar",
+        # "openai/summarize_from_feedback",
+        "stanfordnlp/SHP",
+        # "berkeley-nest/Nectar",
     ]
     for dataset in datasets:
         df = _get_text_ratings(dataset, model=model)
