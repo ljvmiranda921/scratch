2b4cef2cb804574e8df2890b1b2b3a2456428501
Lj Miranda <ljvmiranda@gmail.com>
2024-03-09 15:42:33 -0800

    [wip] Update

--- 2024-02-21-contrast-pairs/scripts/experiment_helpsteer.py
+++ 2024-02-21-contrast-pairs/scripts/experiment_helpsteer.py
@@ -1,14 +1,41 @@
 from pathlib import Path
+from typing import Dict, List, Tuple, Literal
+from operator import attrgetter
 
 import torch
 import typer
 from datasets import load_dataset
 from sentence_transformers import SentenceTransformer
+from scipy.spatial.distance import cosine
 from wasabi import msg
+
+from scripts.preprocessors import compute_elo_rankings
 
 
 def main():
-    pass
+    aspects = ["helpfulness", "correctness", "coherence", "complexity", "verbosity"]
+    for aspect in aspects:
+        chosen_texts, sorted_rejected = _preprocess_helpsteer(aspect)
+
+
+def _preprocess_helpsteer(aspect: str) -> Tuple[List[Tuple], List[List[Tuple]]]:
+    dataset = load_dataset("nvidia/HelpSteer", split="train")
+    df = dataset.to_pandas()
+
+    chosen_texts = []
+    rejected_texts = []
+    for _, instance in df.groupby("prompt"):
+        instance = instance[["response", aspect]]
+        responses = sorted(
+            list(instance.itertuples(index=False)),
+            key=attrgetter(aspect),
+            reverse=True,
+        )
+
+        chosen_texts.append(responses[0])
+        rejected_texts.append(responses[1:])
+
+    return chosen_texts, rejected_texts
 
 
 if __name__ == "__main__":
