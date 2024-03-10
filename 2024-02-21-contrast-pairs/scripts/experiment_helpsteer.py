fbaa05c0ebe4242ee87066be1078299ee0fd30cb
Lj Miranda <ljvmiranda@gmail.com>
2024-03-09 17:09:23 -0800

    [wip] Update

--- 2024-02-21-contrast-pairs/scripts/experiment_helpsteer.py
+++ 2024-02-21-contrast-pairs/scripts/experiment_helpsteer.py
@@ -21,23 +21,23 @@ def main():
 
     aspects = ["helpfulness", "correctness", "coherence", "complexity", "verbosity"]
     for aspect in aspects:
-        output_dir = output_dir / aspect
-        output_dir.mkdir(parents=True, exist_ok=True)
+        aspect_dir = output_dir / aspect
+        aspect_dir.mkdir(parents=True, exist_ok=True)
         msg.text(f"Processing for '{aspect}' aspect")
         chosen_texts, sorted_rejected = _preprocess_helpsteer(aspect)
 
         chosen_embs = model.encode([text.response for text in chosen_texts])
-        np.save(output_dir / f"chosen_{aspect}.npy", chosen_embs)
+        np.save(aspect_dir / f"chosen_{aspect}.npy", chosen_embs)
         for rejection_type in ("next", "mid", "last"):
             if rejection_type == "next":
                 rejected_texts = [rej[0] for rej in sorted_rejected]
             if rejection_type == "last":
                 rejected_texts = [rej[-1] for rej in sorted_rejected]
             if rejection_type == "mid":
-                mid_idx = math.ceil(np.mean(np.arange(len(sorted_rejected))))
-                rejected_texts = [rej[mid_idx] for rej in sorted_rejected]
+                mid_idx = lambda x: math.ceil(np.mean(np.arange(len(x))))  # noqa
+                rejected_texts = [rej[mid_idx(rej)] for rej in sorted_rejected]
             rejected_embs = model.encode([text.response for text in rejected_texts])
-            np.save(output_dir / f"chosen_{aspect}_{rejection_type}.npy", rejected_embs)
+            np.save(aspect_dir / f"chosen_{aspect}_{rejection_type}.npy", rejected_embs)
 
 
 def _preprocess_helpsteer(aspect: str) -> Tuple[List[Tuple], List[List[Tuple]]]:
