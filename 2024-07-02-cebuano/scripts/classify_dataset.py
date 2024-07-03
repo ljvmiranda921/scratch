d589a5beeebd09aabd49109eda81ec083635c5dc
Lj Miranda <ljvmiranda@gmail.com>
2024-07-02 19:50:49 -0700

    [wip] Update

--- 2024-07-02-cebuano/scripts/classify_dataset.py
+++ 2024-07-02-cebuano/scripts/classify_dataset.py
@@ -1,8 +1,7 @@
-from typing import Optional
-
 import typer
 import torch
 from transformers import pipeline
+from dataset import load_dataset
 from wasabi import msg
 
 id2label = {
@@ -15,15 +14,18 @@ id2label = {
 def main(
     # fmt: off
     dataset: str = typer.Argument(..., help="Dataset to test on."),
-    split: Optional[str] = typer.Option("train", help="Split to get the instances from."),
-    column_name: Optional[str] = typer.Option("text", help="Column name to source the texts."),
+    split: str = typer.Option("train", help="Split to get the instances from."),
+    column_name: str = typer.Option("text", help="Column name to source the texts."),
+    model: str = typer.Option("SEACrowd/mdeberta-v3_sea_translationese", help="Classifier to use."),
     # fmt: on
 ):
     """Get distribution of bot-like texts in a dataset"""
+    msg.info("Loading the dataset")
+
     msg.info("Loading classification model")
     pipe = pipeline(
         "text-classification",
-        model="SEACrowd/mdeberta-v3_sea_translationese",
+        model=model,
         device="cuda:0" if torch.cuda.is_available() else "cpu",
     )
     breakpoint()
