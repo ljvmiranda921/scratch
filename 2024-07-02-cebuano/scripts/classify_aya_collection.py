6494c29b74668013567fa77197ff0dff5246b938
Lj Miranda <12949683+ljvmiranda921@users.noreply.github.com>
2026-03-20 10:28:06 +0000

    Add Cebuano explorations (#27)

---
+++ 2024-07-02-cebuano/scripts/classify_aya_collection.py
@@ -0,0 +1,58 @@
+import json
+from collections import Counter
+from pathlib import Path
+from typing import Optional
+
+import typer
+from datasets import load_dataset
+from transformers import pipeline
+from wasabi import msg
+
+id2label = {
+    "LABEL_0": "Human-translated",
+    "LABEL_1": "Machine-translated",
+    "LABEL_2": "Natural",
+}
+
+
+def main(
+    # fmt: off
+    output_path: Path = typer.Argument(..., help="Path to save the statistics as JSON file."),
+    split: str = typer.Option("train", help="Split to get the instances from."),
+    sample: Optional[int] = typer.Option(None, help="If set, will select random instances."),
+    seed: int = typer.Option(42, help="Random seed for shuffling."),
+    # fmt: on
+):
+    """Get distribution of bot-like texts in the Aya Collection dataset"""
+    msg.info("Loading the dataset")
+    dataset_name = "CohereForAI/aya_collection_language_split"
+    dataset = load_dataset(dataset_name, name="cebuano", split=split)
+    if sample:
+        msg.text(f"Getting {sample} samples (seed={seed})")
+        dataset = dataset.shuffle(seed=seed).select(range(sample))
+    df = dataset.to_pandas()
+    print(df["dataset_name"].value_counts().to_markdown(tablefmt="github"))
+
+    msg.info("Loading classification model")
+    model_name = "SEACrowd/mdeberta-v3_sea_translationese"
+    pipe = pipeline(
+        "text-classification",
+        model=model_name,
+        device="cuda:0",
+        batch_size=8,
+    )
+
+    preds_dataset = {}
+    for name, group_df in df.groupby("dataset_name"):
+        texts = group_df["inputs"].to_list()
+        msg.text(f"Classifying {name} dataset (len={len(texts)})")
+        preds = [id2label[pred.get("label")] for pred in pipe(texts)]
+        preds_dataset[name] = Counter(preds)
+
+    preds_json = {k: dict(v) for k, v in preds_dataset.items()}
+    with open(output_path, "w") as f:
+        json.dump(preds_json, f, indent=4)
+
+
+if __name__ == "__main__":
+    typer.run(main)
